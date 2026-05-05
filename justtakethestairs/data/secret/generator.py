17f13e0ea5a77077434cdd49b4346058f4a3c992
Matthias Nielsen <matthias.s.nielsen@protonmail.com>
2026-05-05 21:11:04 +0200

    take-stairs: add testcases + update generator

--- justtakethestairs/data/secret/generator.py
+++ justtakethestairs/data/secret/generator.py
@@ -2,8 +2,11 @@
 import random
 import sys
 
-N = 200_000
-C = 50
+def random_floor() -> int:
+    """
+    Returns a random floor
+    """
+    return random.randint(1, 5)
 
 def min_group_random_floor():
     """
@@ -12,9 +15,11 @@ def min_group_random_floor():
     Should be the worst case <for implementation, since (atleast for my implementation)
     would have to walk back 50 positions before capacity runs out.
     """
+    N = 200_000
+    C = 50
     print(N, C)
     for _ in range(N):
-        print(1, random.randint(1, 5))
+        print(1, random_floor())
 
 def min_group_max_floor():
     """
@@ -24,6 +29,17 @@ def min_group_max_floor():
     43_995 (per trip: 5 (up) + 1 (stop) + 5 (down), except for last.
     4_000 trip totals = 43_995.
     """
+    N = 200_000
+    C = 50
     print(N, C)
     for _ in range(N):
         print(1, 5)
+
+def min_capacity_max_groups():
+    N = 200_000
+    C = 1
+    print(N, C)
+    for _ in range(N):
+        print(1, random_floor())
+
+min_capacity_max_groups()
