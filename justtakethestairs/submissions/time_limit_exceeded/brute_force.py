4ce2e2ccd74862e9f668b2de293ca654a0e89299
Matthias Nielsen <matthias.s.nielsen@protonmail.com>
2026-05-08 14:35:14 +0200

    take-stairs: make TLE submission iterative to avoid RTE

--- justtakethestairs/submissions/time_limit_exceeded/brute_force.py
+++ justtakethestairs/submissions/time_limit_exceeded/brute_force.py
@@ -1,26 +1,22 @@
 #The basic idea of the brute force solution is:
 #"Try every possible way to split the groups into trips, take the minimum cost".
-# It's basicly top-down recursion DP without memorization (so full-search).
-
-import sys
-
-sys.setrecursionlimit(100_000)
 
 N, C = map(int, input().split())
-S = [0] * (N + 1)  # 1-indexed
-F = [0] * (N + 1)  # 1-indexed
+S = [0] * (N + 1)
+F = [0] * (N + 1)
 for i in range(1, N + 1):
     S[i], F[i] = map(int, input().split())
 
-
-# Try every way to split groups [start..N] into trips. Return min cost.
-def solve(start):
+best = float("inf")
+stack = [(1, 0)]
+while stack:
+    start, cost = stack.pop()
     if start > N:
-        return 0
-    best = float("inf")
+        best = min(best, cost)
+        continue
     students = 0
     floors = set()
-    for end in range(start, N + 1):  # current trip is groups [start..end]
+    for end in range(start, N + 1):
         students += S[end]
         if students > C:
             break
@@ -29,9 +25,6 @@ def solve(start):
         stops = len(floors)
         trip_cost = max_floor + stops + max_floor
         if end == N:
-            trip_cost -= max_floor  # Elevator doesn't go down on last trip
-        best = min(best, trip_cost + solve(end + 1))
-    return best
-
-
-print(solve(1))
+            trip_cost -= max_floor
+        stack.append((end + 1, cost + trip_cost))
+print(best)
